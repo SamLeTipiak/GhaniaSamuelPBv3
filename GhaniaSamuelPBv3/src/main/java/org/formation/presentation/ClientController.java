8ecae1013f6a0edd06b61c10a3a88da847e351b0
ghania <ghania.bouzemame@gmail.com>
2017-03-30 14:23:10 +0200

    ok

--- GhaniaSamuelPBv3/src/main/java/org/formation/presentation/ClientController.java
+++ GhaniaSamuelPBv3/src/main/java/org/formation/presentation/ClientController.java
@@ -1,6 +1,55 @@
 package org.formation.presentation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.formation.beans.Client;
+import org.formation.service.IServiceClient;
+
 public class ClientController {
+
+	@Inject
+	IServiceClient serviceClient;
+	private List<Client> clients = new ArrayList<>();
+	Client client = new Client();
+
+	public ClientController(List<Client> clients) {
+		super();
+		this.clients = clients;
+	}
+
+	public void createClient(Client client) {
+
+	}
+
+	public void updateClient(Client client) {
+
+	}
+
+	public Client readClient(long idClient) {
+		return client;
+
+	}
+
+	public List<Client> readList() {
+		return clients;
+	}
 	
+	public void loadClients() {
+		clients.clear();
+		
+		try {
+			clients = serviceClient.readList();
+			
+		} catch (Exception e) {
+			
+		}
+	}
+
+	public void deleteClient(long idClient) {
+
+	}
 
 }
